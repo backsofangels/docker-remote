3565e430130e91c02c78ce7566377b09599091d8
Salvatore <sal.penitente@gmail.com>
2023-03-18 10:07:48 +0100

    FIrst commit

---
+++ src/main/java/com/backsofangels/dockerremote/DockerRemoteApplication.java
@@ -0,0 +1,13 @@
+package com.backsofangels.dockerremote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerRemoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerRemoteApplication.class, args);
+	}
+
+}
