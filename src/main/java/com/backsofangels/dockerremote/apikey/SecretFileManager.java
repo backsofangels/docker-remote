5a2345b459f192a168d8f74ed69ef67b9b519f12
Salvatore <sal.penitente@gmail.com>
2023-03-19 10:55:31 +0100

    Add SecretFileManager unit tests

---
+++ src/main/java/com/backsofangels/dockerremote/apikey/SecretFileManager.java
@@ -0,0 +1,60 @@
+package com.backsofangels.dockerremote.apikey;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SecretFileManager {
+    private final String FILE_NAME = "src/main/resources/apikey";
+    private Logger logger = LoggerFactory.getLogger("SecretFileManager");
+
+    public SecretFileManager() {}
+
+    public boolean createApiKeyFile() {
+        try {
+            Path pathToSecret = Paths.get(FILE_NAME);
+            Files.createFile(pathToSecret);
+            logger.debug("File created");
+            return true;
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean checkIfApiKeyExists() {
+        return Files.exists(Paths.get(FILE_NAME));
+    }
+
+    public boolean persistApiKeyOnFile(String keyString) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME));
+            writer.write(keyString);
+            writer.close();
+            return true;
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+            return false;
+        }
+    }
+
+    public String retrieveKeyFromFile() {
+        if (!checkIfApiKeyExists()) {
+            return null;
+        } else {
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME));
+                String key = reader.readLine();
+                reader.close();
+                return key;
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+                return null;
+            }
+        }
+    }
+}
