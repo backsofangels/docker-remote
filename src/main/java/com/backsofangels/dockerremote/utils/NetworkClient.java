31d017de7edab4f09dc47393e0604834daa439db
Salvatore <sal.penitente@gmail.com>
2023-03-26 23:22:24 +0200

    add common network client

--- src/main/java/com/backsofangels/dockerremote/utils/NetworkClient.java
+++ src/main/java/com/backsofangels/dockerremote/utils/NetworkClient.java
@@ -1,2 +1,28 @@
-package com.backsofangels.dockerremote.utils;public class NetworkClient {
+package com.backsofangels.dockerremote.utils;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class NetworkClient {
+    private CloseableHttpClient client;
+    private static NetworkClient networkClient;
+
+    public synchronized static NetworkClient getNetworkClient() {
+        if (networkClient == null) {
+            return new NetworkClient();
+        } else return networkClient;
+    }
+
+    private NetworkClient() {
+        this.client = initClient();
+    }
+
+    private CloseableHttpClient initClient() {
+        CloseableHttpClient localClient = HttpClients.createDefault();
+        return localClient;
+    }
+
+    public CloseableHttpClient getClient() {
+        return this.client;
+    }
 }
