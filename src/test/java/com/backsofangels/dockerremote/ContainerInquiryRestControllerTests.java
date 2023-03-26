a55090facd8428b42b3265cae8a1d94ed417f578
Salvatore <sal.penitente@gmail.com>
2023-03-26 23:32:17 +0200

    Unit test for runningContainers

--- src/test/java/com/backsofangels/dockerremote/ContainerInquiryRestControllerTests.java
+++ src/test/java/com/backsofangels/dockerremote/ContainerInquiryRestControllerTests.java
@@ -1,2 +1,24 @@
-package com.backsofangels.dockerremote;public class ContainerInquiryRestControllerTests {
+package com.backsofangels.dockerremote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ContainerInquiryRestControllerTests {
+
+    @Value(value="${local.server.port}")
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    void getRunningContainersTest() {
+        assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/"
+        + "containers/running", String.class)).isNull();
+    }
 }
