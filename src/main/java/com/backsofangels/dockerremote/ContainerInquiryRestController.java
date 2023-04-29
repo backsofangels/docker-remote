a72ad409ffa4c26a9f20899cea33fdc88ad8e14b
Salvatore <sal.penitente@gmail.com>
2023-04-29 12:31:11 +0200

    Add swagger documentation

--- src/main/java/com/backsofangels/dockerremote/ContainerInquiryRestController.java
+++ src/main/java/com/backsofangels/dockerremote/ContainerInquiryRestController.java
@@ -2,6 +2,11 @@ package com.backsofangels.dockerremote;
 
 import com.backsofangels.dockerremote.services.ContainersInfoService;
 import com.backsofangels.dockerremote.model.Container;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +17,17 @@ import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
-@RequestMapping("containers")
+@RequestMapping("containers/")
 public class ContainerInquiryRestController {
     @Autowired
     private ContainersInfoService containersInfoService;
 
+    @Operation(summary = "Get all running containers")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retrieves all running container",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Container.class)) })
+    })
     @GetMapping(value = "running")
     public List<Container> getRunningContainers() throws URISyntaxException, IOException {
         return containersInfoService.getRunningContainers();
--- src/main/java/com/backsofangels/dockerremote/constants/LocalhostConfiguration.java
+++ src/main/java/com/backsofangels/dockerremote/constants/LocalhostConfiguration.java
@@ -3,6 +3,6 @@ package com.backsofangels.dockerremote.constants;
 public class LocalhostConfiguration {
     public static final String LOCALHOST_SCHEME = "http";
     public static final String LOCALHOST_HOST = "localhost";
-    public static final int LOCALHOST_PORT = 3001;
+    public static final int LOCALHOST_PORT = 2375;
 
 }
